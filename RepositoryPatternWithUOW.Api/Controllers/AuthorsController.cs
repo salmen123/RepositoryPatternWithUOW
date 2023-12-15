f97b04e9338ee44e5f55d6c31344b1d6344061b4
Med Salmen Saadi <medsalmen.saadi@esprit.tn>
2023-12-15 11:43:32 -0500

    Test The Base (Generic) Repository

---
+++ RepositoryPatternWithUOW.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternWithUOW.Core.Interfaces;
+using RepositoryPatternWithUOW.Core.Models;
+
+namespace RepositoryPatternWithUOW.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IBaseRepository<Author> _authorsRepository;
+
+        public AuthorsController(IBaseRepository<Author> authorsRepository)
+        {
+            _authorsRepository = authorsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetById()
+        {
+            return Ok(_authorsRepository.GetById(1));
+        }
+    }
+}
---
+++ RepositoryPatternWithUOW.Api/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternWithUOW.Core.Interfaces;
+using RepositoryPatternWithUOW.Core.Models;
+
+namespace RepositoryPatternWithUOW.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBaseRepository<Book> _booksRepository;
+
+        public BooksController(IBaseRepository<Book> booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetById()
+        {
+            return Ok(_booksRepository.GetById(1));
+        }
+    }
+}
